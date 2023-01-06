d7dfdf322b0d1c8b7b8378465b161c68bd2fb14a
mitchelcool <mitcheinaju@punch.cool>
2023-01-06 06:08:49 +0100

    working on shared notes

--- src/components/SharedNote.jsx
+++ src/components/SharedNote.jsx
@@ -30,37 +30,37 @@ function SharedNote() {
   useEffect(() => {
     handleGetSpecificNote();
   }, []);
-  return <div className="bg-darkPrimary text-white h-screen overflow-scroll ">
-{specificNoteData ?
-<div className="m-10">
-  <h1 className="bg-inherit w-full focus:outline-none placeholder:text-2xl text-2xl">
-    {specificNoteData.title}
-    </h1>
-    <p className="bg-inherit w-full h-[70vh] text-white/[0.8] focus:outline-none pt-4 placeholder:text-xl text-xl">
-      {specificNoteData.note}
-    </p>
-    <p className="text-white/[0.8] text-sm ">
-      <i>
-        last edited:   {getDateFormat(specificNoteData?.timeStamp)}
-      {/* {specificNoteData.timeStamp} */}
-      </i>
-      <br />
-      author: {noteOwner}
-    </p>
+  return (
+    <div className="bg-darkPrimary text-white h-screen overflow-scroll scrollbar-hide  ">
+      {specificNoteData ? (
+        <div className="m-10">
+          <h1 className="bg-inherit w-full focus:outline-none placeholder:text-2xl text-2xl">
+            {specificNoteData.title}
+          </h1>
+          <p className="bg-inherit w-full h-[70vh] text-white/[0.8] focus:outline-none pt-4 placeholder:text-xl text-xl">
+            {specificNoteData.note}
+          </p>
+          <p className="text-white/[0.8] text-sm ">
+            <i>
+              last edited: {getDateFormat(specificNoteData?.timeStamp)}
+              {/* {specificNoteData.timeStamp} */}
+            </i>
+            <br />
+            author: {noteOwner}
+          </p>
 
-      <div className="mt-2">
-
-      <ButtonComponent text="create your shareable note" onClick={()=>window.location='/login'} />
-      </div>
-</div>
-:
-<>
-Please Wait
-</>
-}
-  
-  
-  </div>;
+          <div className="mt-2">
+            <ButtonComponent
+              text="create your shareable note"
+              onClick={() => (window.location = "/login")}
+            />
+          </div>
+        </div>
+      ) : (
+        <>Please Wait</>
+      )}
+    </div>
+  );
 }
 
 export default SharedNote;
