26ac8c3b8d42b626419081cb16733aa0e9d3d5bb
mitchelcool <mitcheinaju@punch.cool>
2023-01-06 08:14:33 +0100

    worked on the sharing note bug

--- src/components/SharedNote.jsx
+++ src/components/SharedNote.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect,useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom";
 import { doc, getDoc } from "firebase/firestore";
 import { db } from "../firebase-config";
@@ -9,8 +9,8 @@ import { removeTags } from "../utils/removeTags";
 
 function SharedNote() {
   const [searchParams, setSearchParams] = useSearchParams();
-  const [specificNoteData,setSpecificNoteDate]=useState()
-  const [noteOwner,setNoteOwner]=useState()
+  const [specificNoteData, setSpecificNoteData] = useState();
+  const [noteOwner, setNoteOwner] = useState();
   // console.log(searchParams.get("noteId"));
   const docRef = doc(db, "note", searchParams.get("noteId"));
   const handleGetSpecificNote = async () => {
@@ -18,10 +18,11 @@ function SharedNote() {
       const docSnap = await getDoc(docRef);
       if (docSnap.exists()) {
         // console.log(docSnap.data());
-        removeTags(docSnap.data())
-        setSpecificNoteDate(docSnap.data());
-         const ownerData=await getUser(docSnap.data().user)
-         setNoteOwner(ownerData[0]?.name)
+        console.log(removeTags(docSnap.data().note), "remove tags");
+        setSpecificNoteData(docSnap.data());
+        // setSpecificNoteData(docSnap.data());
+        const ownerData = await getUser(docSnap.data().user);
+        setNoteOwner(ownerData[0]?.name);
       } else {
         console.log("Document does not exist");
       }
@@ -39,23 +40,32 @@ function SharedNote() {
           <h1 className="bg-inherit w-full focus:outline-none placeholder:text-2xl text-2xl">
             {specificNoteData.title}
           </h1>
-          <p className="bg-inherit w-full h-[70vh] text-white/[0.8] focus:outline-none pt-4 placeholder:text-xl text-xl">
-            {specificNoteData.note}
+          <p className="bg-inherit w-full h-[60vh] mb-20 text-white/[0.8] focus:outline-none pt-4 placeholder:text-xl text-xl">
+            {/* {removeTags(specificNoteData.note)} */}
+            <div dangerouslySetInnerHTML={{ __html: specificNoteData.note }} />
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+          
           </p>
-          <p className="text-white/[0.8] text-sm ">
-            <i>
-              last edited: {getDateFormat(specificNoteData?.timeStamp)}
-              {/* {specificNoteData.timeStamp} */}
-            </i>
-            <br />
-            author: {noteOwner}
-          </p>
+          <div className="absolute z-10">
+            <p className="text-white/[0.8] text-sm ">
+              <i>
+                last edited: {getDateFormat(specificNoteData?.timeStamp)}
+                {/* {specificNoteData.timeStamp} */}
+              </i>
+              <br />
+              author: {noteOwner}
+            </p>
 
-          <div className="mt-2">
-            <ButtonComponent
-              text="create your shareable note"
-              onClick={() => (window.location = "/login")}
-            />
+            <div className="mt-2">
+              <ButtonComponent
+                text="create your shareable note"
+                onClick={() => (window.location = "/login")}
+              />
+            </div>
           </div>
         </div>
       ) : (
