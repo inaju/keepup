b910d4e084c5da3c6dfe4d9c9dfb83139ab7febf
mitchelcool <mitcheinaju@punch.cool>
2023-01-07 19:30:06 +0100

    worked on the landing page

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,13 +3,13 @@ import { AiOutlineArrowRight } from 'react-icons/ai'
 import hero_image from "../assets/hero_image_1.png"
 function Hero() {
     return (
-        <div className=' Poppins text-white  m-auto w-screen  overflow-none absolute top-[20%] left-[5%] translate-x-[1%] translate-y-[50%] sm:top-[1%] sm:translate-y-[25%] lg:translate-y-[20%]  lg:translate-x-[0%] lg:left-[0%] lg:w-full'>
+        <div className=' Poppins text-white  m-auto w-screen  overflow-none absolute top-[20%] left-[5%] translate-x-[1%] translate-y-[50%] sm:top-[1%] sm:translate-y-[25%] lg:translate-y-[25%]  lg:translate-x-[0%] lg:left-[0%] lg:w-full'>
             <div className='scrollbar-hide overflow-none w-screen sm:mx-auto   max-w-sm sm:flex items-center flex-col sm:max-w-xl lg:w-full lg:max-w-4xl'>
 
-                <h1 className='text-left text-2xl sm:text-center sm:text-3xl lg:text-4xl lg:w-full lg:text-center lg:tracking-wide '>
+                <h1 className='text-left text-2xl sm:text-center sm:text-3xl lg:text-4xl lg:w-full lg:text-center lg:tracking-normal lg:font-light'>
                     <p className='leading-normal'>
 
-                    Boost your productivity with our &nbsp; 
+                    Boost your productivity with our&nbsp; 
                     <b className='font-sembbold'>
 
                         streamlined <br/> note-taking  &nbsp;
@@ -21,7 +21,7 @@ function Hero() {
                 <p className='text-white/[0.6] max-w-md text-sm mt-6 sm:text-center lg:text-md'>
                     Effortlessly access your notes from any device with our convenient cloud-based solution.
                 </p>
-                <button onClick={()=>window.location="/app"} className='bg-[#3A3AF4] hover:bg-[#3A3AF4]/[0.7] active:ring-4 active:ring-offset-1 text-white p-3 mt-4 pl-6 pr-6 rounded-md flex items-center justify-between'>
+                <button onClick={()=>window.location="/app"} className='bg-cyan-500 hover:bg-cyan-500/[0.7] active:ring-4 active:ring-offset-1 text-white p-3 mt-6 pl-6 pr-6 rounded-md flex items-center justify-between'>
                     <p className='text-sm mr-2 '>
                         Try For Free
                     </p>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,34 +1,85 @@
-import React,{useState} from 'react'
-import { RxHamburgerMenu } from "react-icons/rx"
-import { AiOutlineClose } from "react-icons/ai"
+import React, { useState } from "react";
+import { RxHamburgerMenu } from "react-icons/rx";
+import { AiOutlineClose } from "react-icons/ai";
 function NavBar() {
-    const [show,setShow]=useState(false)
-    return (
-        <div className='absolute top-0 flex items-center justify-between w-full p-4 text-white lg:px-20 '>
-            <h1 className="logo px-4 py-2 text-2xl">Keepup</h1>
+  const [show, setShow] = useState(false);
+  return (
+    <div className="absolute top-0 flex items-center justify-between w-full p-4 text-white lg:px-20 ">
+      <h1 className="logo px-4 py-2 text-2xl">Keepup</h1>
 
-            <div className={show ? 'hidden' :'block'} onClick={()=>setShow(!show)}>
-
-                <RxHamburgerMenu size={30}  />
-            </div>
-            <div className={!show ? 'hidden' :'block'} onClick={()=>setShow(!show)}>
-
-                <AiOutlineClose size={25} />
-            </div>
-            <ul className='hidden'>
-                <li>
-
-                    <a
-                        href="https://github.com/oslabs-beta/oslabs-beta"
-                        target="_blank"
-                        rel="noreferrer"
-                    >
-                        Github
-                    </a>
-                </li>
-            </ul>
+      <div className="sm:hidden">
+        <div
+          className={show ? "hidden" : "block"}
+          onClick={() => setShow(!show)}
+        >
+          <RxHamburgerMenu size={30} />
         </div>
-    )
+        <div
+          className={!show ? "hidden" : "block  "}
+          onClick={() => setShow(!show)}
+        >
+          <AiOutlineClose size={25} />
+        </div>
+      </div>
+      <ul className="hidden lg:flex lg:flex-row  lg:items-start lg:justify-between lg:w-full lg:max-w-sm  ">
+        <li className="active:font-semibold active:text-white hover:text-white text-white/[0.6]">
+          <a href="" rel="noreferrer">
+            Home
+          </a>
+        </li>
+        <li className="active:font-semibold active:text-white hover:text-white text-white/[0.6]">
+          <a href="" rel="noreferrer">
+            Product
+          </a>
+        </li>
+        <li className="active:font-semibold active:text-white hover:text-white text-white/[0.6]">
+          <a href="" rel="noreferrer">
+            About
+          </a>
+        </li>
+      </ul>
+      <button
+        onClick={() => (window.location = "/login")}
+        className="hidden lg:flex p-2 px-6 shadow-lg bg-black/[0.3] rounded-md text-white font-semibold hover:text-white hover:bg-cyan-500"
+      >
+        Login
+      </button>
+      {show && (
+        <div
+          onClick={() => setShow(!show)}
+          className="absolute top-0 left-0 h-screen w-screen z-10 bg-cyan-500"
+        >
+          <div
+            className={!show ? "hidden" : "block right-4 top-6 absolute "}
+            onClick={() => setShow(!show)}
+          >
+            <AiOutlineClose size={25} />
+          </div>
+          <ul className="text-xl mt-20 flex items-center flex-col justify-between  h-1/3 ">
+            <li className="active:font-semibold hover:text-white/[0.6]">
+              <a href="/" rel="noreferrer">
+                Home
+              </a>
+            </li>
+            <li className="active:font-semibold hover:text-white/[0.6]">
+              <a
+                href="/login"
+                // target="_blank"
+                rel="noreferrer"
+              >
+                Product
+              </a>
+            </li>
+            <li className="active:font-semibold hover:text-white/[0.6]">
+              <a href="" rel="noreferrer">
+                About
+              </a>
+            </li>
+          </ul>
+        </div>
+      )}
+    </div>
+  );
 }
 
-export default NavBar
+export default NavBar;
